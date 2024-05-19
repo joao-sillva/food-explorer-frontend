39328d65c20accbb8160a2873e52a739bafabbb5
joao-sillva <joao.vgsillva@gmail.com>
2024-05-19 19:06:09 -0300

    finish desktop SignUp

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,9 +1,48 @@
-import { Container } from './styles'
+import { Link } from 'react-router-dom'
+import { Container, Form, Brand } from './styles'
+import { Section } from '../../components/Section'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import brand from '../../assets/brand.svg'
 
 export function SignUp() {
   return (
     <Container>
-      
+      <Brand>
+        <img src={brand} alt="Logo Food Explorer" />
+      </Brand>
+
+      <Form>
+        <h2>Crie sua conta</h2>
+
+        <Section title='Seu nome'>
+          <Input 
+            placeholder='Exemplo: João da Silva'
+            type='text'
+          />
+        </Section>
+
+        <Section title='Email'>
+          <Input 
+            placeholder='Exemplo: exemplo@exemplo.com.br'
+            type='email'
+          />
+        </Section>
+
+        <Section title='Senha'>
+          <Input 
+            placeholder='No mínimo 6 caracteres'
+            type='password'
+          />
+        </Section>
+
+        <Button title='Criar conta' />
+
+        <Link to='/'>
+          Já tenho uma conta
+        </Link>
+      </Form>
     </Container>
   )
 }
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,5 +1,55 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+  height: 100vh;
+  margin-inline: 154px 108px;
 
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const Brand = styled.div`
+  display: flex;
+  
+  > svg {
+    width: 324px;
+    height: 48px;
+  }
+`
+
+export const Form = styled.form`
+  min-width: 476px;
+  padding: 64px;
+  border-radius: 16px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  background: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+
+  > h2 {
+    font-family: 'Poppins', sans-serif;
+    font-size: 32px;
+    font-weight: 500;
+    line-height: 140%;
+    text-align: center;
+  }
+  
+  > section h2 {
+    margin-bottom: 8px;
+  }
+
+  > section input {
+    background: ${({ theme }) => theme.COLORS.BACKGROUND_500};
+    border-radius: 8px;
+  }
+  
+  > a {
+    font-family: 'Poppins', sans-serif;
+    font-size: 14px;
+    line-height: 24px;
+    text-align: center;
+    color: ${({ theme }) => theme.COLORS.WHITE};
+  }
 `
