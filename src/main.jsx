0bb69454db6e6fcb4cc5c512fca439157b3a083e
joao-sillva <joao.vgsillva@gmail.com>
2024-05-19 13:58:10 -0300

    set styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import theme from './styles/theme'
+import GlobalStyles from './styles/global'
+import { ThemeProvider } from 'styled-components'
+
 import { SignIn } from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SignIn />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <SignIn />
+    </ThemeProvider>
   </React.StrictMode>,
 )
