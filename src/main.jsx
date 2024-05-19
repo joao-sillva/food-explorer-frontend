3797bd7fe877afeab582b949bfcc8988a6e2d982
joao-sillva <joao.vgsillva@gmail.com>
2024-05-19 14:41:30 -0300

    create page New

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import theme from './styles/theme'
 import GlobalStyles from './styles/global'
 import { ThemeProvider } from 'styled-components'
 
-import { SignIn } from './pages/SignIn'
+import { New } from './pages/New'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignIn />
+      <New />
     </ThemeProvider>
   </React.StrictMode>,
 )
