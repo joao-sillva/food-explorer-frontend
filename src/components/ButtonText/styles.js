68e57ddf619de9f0876c80453482c373b73c36b6
joao-sillva <joao.vgsillva@gmail.com>
2024-05-19 16:15:30 -0300

    add component ButtonText

---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Container = styled.button`
+  background: none;
+  border: none;
+  color: ${({ theme }) => theme.COLORS.GRAY_200};
+
+  font-family: 'Poppins', sans-serif;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 140%;
+
+  display: flex;
+  align-items: center;
+
+  > svg {
+    width: 32px;
+    height: 32px;
+    color: ${({ theme }) => theme.COLORS.WHITE};
+  }
+`
--- src/pages/Dish/index.jsx
+++ src/pages/Dish/index.jsx
@@ -1,12 +1,19 @@
 import { Container } from './styles'
+import { RxCaretLeft } from 'react-icons/rx'
 import { Header } from '../../components/Header'
 import { Footer } from '../../components/Footer'
 import { Tag } from '../../components/Tag'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Dish() {
   return (
     <Container>
       <Header />
+
+      <ButtonText>
+        <RxCaretLeft />
+        Voltar
+      </ButtonText>
 
       <Tag title="alface" />
       <Tag title="cebola" />
