2d4765b4476854c22ebf778d17f76288c4835c19
joao-sillva <joao.vgsillva@gmail.com>
2024-05-20 05:32:44 -0300

    finish desktop and mobile Home

--- src/styles/global.js
+++ src/styles/global.js
@@ -8,6 +8,11 @@ export default createGlobalStyle`
     -moz-font-smoothing: antialiased;
   }
 
+  :root {
+    --swiper-navigation-size: 40px;
+    --swiper-theme-color: ${({ theme }) => theme.COLORS.WHITE};
+  }
+
   body {
     background: ${({ theme }) => theme.COLORS.BACKGROUND_800};
     color: ${({ theme }) => theme.COLORS.WHITE};
