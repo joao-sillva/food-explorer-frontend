cd86451c9dbed1ee6c5b10f551cdaf7dd8865ecc
joao-sillva <joao.vgsillva@gmail.com>
2024-05-20 03:28:54 -0300

    finish mobile SignUp

--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -2,38 +2,51 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   height: 100vh;
-  margin-inline: 154px 108px;
+  margin-inline: 65px 47px;
 
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
+  gap: 73px;
+
+  @media (min-width: 1024px) {
+    margin-inline: 154px 108px;
+    flex-direction: row;
+    justify-content: space-between;
+    gap: 0;
+  }
 `
 
 export const Brand = styled.div`
   display: flex;
-  
+  margin-inline: auto;
+
   > svg {
-    width: 324px;
-    height: 48px;
+    width: 278px;
+    height: 44px;
+  }
+
+  @media (min-width: 1024px) {
+    margin-inline: 0;
+   
+    > svg {
+      width: 324px;
+      height: 48px;
+    }
   }
 `
 
 export const Form = styled.form`
-  min-width: 476px;
-  padding: 64px;
-  border-radius: 16px;
+  width: 100%;
+  max-width: 316px;
 
   display: flex;
   flex-direction: column;
   gap: 32px;
-  background: ${({ theme }) => theme.COLORS.BACKGROUND_700};
 
   > h2 {
-    font-family: 'Poppins', sans-serif;
-    font-size: 32px;
-    font-weight: 500;
-    line-height: 140%;
-    text-align: center;
+    display: none;
   }
   
   > section h2 {
@@ -51,5 +64,21 @@ export const Form = styled.form`
     line-height: 24px;
     text-align: center;
     color: ${({ theme }) => theme.COLORS.WHITE};
+  }
+
+  @media (min-width: 1024px) {
+    min-width: 476px;
+    padding: 64px;
+    border-radius: 16px;
+    background: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+    
+    > h2 {
+      display: initial;
+      font-family: 'Poppins', sans-serif;
+      font-size: 32px;
+      font-weight: 500;
+      line-height: 140%;
+      text-align: center;
+    }
   }
 `
