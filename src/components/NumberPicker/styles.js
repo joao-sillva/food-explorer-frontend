6ffd5c205328adde73d3643bd572e23d37bffec0
joao-sillva <joao.vgsillva@gmail.com>
2024-05-20 03:52:25 -0300

    improve component NumberPicker

--- src/components/NumberPicker/styles.js
+++ src/components/NumberPicker/styles.js
@@ -6,12 +6,23 @@ export const Container = styled.div`
   gap: 14px;
   color: ${({ theme }) => theme.COLORS.GRAY_200};
 
-  svg {
-    cursor: pointer;
+  button {
+    color: ${({ theme }) => theme.COLORS.GRAY_200};
+    background: none;
+    border: none;
     transition: filter .2s;
 
     &:hover {
       filter: brightness(.9);
     }
+
+    @media (min-width: 1024px) {
+    svg,
+    span {
+      font-size: 20px;
+      font-weight: 700;
+      line-height: 160%;
+    }
+  }
   }
 `
