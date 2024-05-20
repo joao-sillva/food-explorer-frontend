ad6a74a384f5985626bebfde98d4e9f2f5152c33
joao-sillva <joao.vgsillva@gmail.com>
2024-05-20 04:10:02 -0300

    add component Menu

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,15 +1,14 @@
-import { FiSearch } from "react-icons/fi";
-import { Container } from "./styles";
-
-import { Input } from "../../components/Input";
+import { Container } from './styles'
+import { FiSearch } from 'react-icons/fi'
+import { Input } from '../../components/Input'
 
 export function Search() {
   return (
     <Container>
       <Input
-        placeholder="Busque por pratos ou ingredientes"
+        placeholder='Busque por pratos ou ingredientes'
         icon={FiSearch}
       />
     </Container>
-  );
+  )
 }
