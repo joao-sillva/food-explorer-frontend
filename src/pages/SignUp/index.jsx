3933b728b139305baf24331b5de1b5f981ba181b
joao-sillva <joao.vgsillva@gmail.com>
2024-05-20 21:04:37 -0300

    add user register

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,7 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { api } from '../../services/api'
+
 import { Container, Form, Brand } from './styles'
 import { Section } from '../../components/Section'
 import { Input } from '../../components/Input'
@@ -7,6 +10,32 @@ import { Button } from '../../components/Button'
 import brand from '../../assets/brand.svg'
 
 export function SignUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert('Preencha todos os campos!');
+    }
+
+    api.post('/users', { name, email, password })
+      .then(() => {
+        alert('Usuário cadastrado com sucesso!')
+        navigate(-1)
+      })
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.message)
+        } else {
+          alert('Não foi possível cadastrar.')
+        }
+      }
+    )
+  }
+
   return (
     <Container>
       <Brand>
@@ -17,27 +46,30 @@ export function SignUp() {
         <h2>Crie sua conta</h2>
 
         <Section title='Seu nome'>
-          <Input 
+          <Input
             placeholder='Exemplo: João da Silva'
             type='text'
+            onChange={e => setName(e.target.value)}
           />
         </Section>
 
         <Section title='Email'>
-          <Input 
+          <Input
             placeholder='Exemplo: exemplo@exemplo.com.br'
             type='email'
+            onChange={e => setEmail(e.target.value)}
           />
         </Section>
 
         <Section title='Senha'>
-          <Input 
+          <Input
             placeholder='No mínimo 6 caracteres'
             type='password'
+            onChange={e => setPassword(e.target.value)}
           />
         </Section>
 
-        <Button title='Criar conta' />
+        <Button title='Criar conta' onClick={handleSignUp} />
 
         <Link to='/'>
           Já tenho uma conta
